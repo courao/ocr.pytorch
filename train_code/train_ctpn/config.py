8970af447f8e415b4cc7bd956a4b5a13d836e9f4
Kyle <s.t.o.r.m.detective@gmail.com>
2021-09-03 18:06:53 -0500

    updata data for google colab training

--- train_code/train_ctpn/config.py
+++ train_code/train_ctpn/config.py
@@ -7,14 +7,14 @@
 import os
 from pathed import filedir
 
-# base_dir = 'path to dataset base dir'
-base_dir = filedir / ".." / ".."
+# ocr_pytorch = 'path to ocr_pytorch'
+ocr_pytorch = filedir / ".." / ".."
 
 
-icdar17_mlt_img_dir = base_dir / "train"
-icdar17_mlt_gt_dir = base_dir / "train_gt"
+icdar17_mlt_img_dir = ocr_pytorch / "ctpn_data" / "train"
+icdar17_mlt_gt_dir = ocr_pytorch / "ctpn_data" / "train_gt"
 num_workers = 0  # change to 2 when on GPU
-pretrained_weights = base_dir / "checkpoints" / "CTPN.pth"
+pretrained_weights = ocr_pytorch / "checkpoints" / "CTPN.pth"
 batch_size = 1
 
 max_epochs = 2  # change to 30 when on GPU
@@ -33,4 +33,4 @@ OHEM = True
 prob_thresh = 0.5
 height = 720
 
-checkpoints_dir = "./checkpoints"
+checkpoints_dir = ocr_pytorch / "checkpoints"
